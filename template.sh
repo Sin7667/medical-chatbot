890f0f8269604e81d11eb0c8dd04537adef408a0
sanaz <sanaz.dt9776@gmail.com>
2026-08-18 14:56:06 +0200

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Create directories for source code and research files
+mkdir -p src
+mkdir -p research
+
+#Creating files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Project structure created successfully."
