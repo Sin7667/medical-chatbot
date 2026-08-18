b9b4aa8a0e7da082cc325759d1fd3d104fe063b4
sanaz <sanaz.dt9776@gmail.com>
2026-08-18 15:15:58 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Sanaz",
+    author_email="sanaz.dt97@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
